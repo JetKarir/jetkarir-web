43bb59ede590e58bce8222580e26af2311bb356e
Muhammad Nafis <m.nafis.2001@gmail.com>
2026-08-09 18:30:22 +0700

    mod: edit env

--- src/env/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  API_URL: 'https://dev.api.jetkarir.com/',
-  OAUTH_GOOGLE_CLIENT_ID:
-    '298084171479-e1u6mmnd1s3lbmbmla86dlo6e012gpji.apps.googleusercontent.com',
-};
---
+++ src/env/environment.example.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  API_URL: 'your_api_url',
+  OAUTH_GOOGLE_CLIENT_ID: 'your_google_client_id',
+};
